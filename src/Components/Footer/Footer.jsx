6d0fa496085652e27a2aa77de119b40da536fa04
Manish <139942794+Manish6906@users.noreply.github.com>
2025-05-01 11:14:43 +0530

    chng logo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -23,7 +23,7 @@ function Footer() {
         <div className='relative z-10 text-white'>
           <div className='md:flex justify-between items-center mb-10'>
             <img src={mainlogo} alt='footer logo' className=" h-12 md:h-15" />
-            <h1 className='text-xl lg:text-3xl font-bold text-center md:text-left'>Schedule Your Rental Today!</h1>
+            <h1 className='text-xl lg:text-3xl font-bold  md:text-left'>Schedule Your Rental Today!</h1>
             <button className="mt-3 sm:mt-0 flex items-center gap-2 px-6 py-2 cursor-pointer bg-[#EF8D2A] rounded-sm hover:bg-[#e67e10] transition-colors">
               Make Appointment <FaArrowRight />
             </button>
